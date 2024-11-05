1637e80221b0e1d6a3d588837c85998638c0e6d6
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-11-05 08:43:50 +0800

    "Added div wrapper with padding, heading, and flexbox layout to App component"

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import { pixels } from './data/pixels'
 
 const App = () => {
   return (
-    <>
+    <div className='p-12'>
+    <h5 className='text-3xl text-center font-medium text-amber-900'>Welcome to nowhere</h5>
+    <div className='flex justify-center flex-wrap gap-8 pt-12'>
       <Frame variant={'sm'} pixel={pixels.RubberDucky} />
       <Frame variant={'sm'} pixel={pixels.Zenitsu} />
       <Frame variant={'sm'} pixel={pixels.Insouke} />
-    </>
+    </div>
+    </div>
   )
 }
 
