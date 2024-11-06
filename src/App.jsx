09476db1735d6637d2dfc8ff1217b1a33393bfa7
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-11-06 10:56:55 +0800

    "Refactor App component to remove hardcoded pixel grid and add Frame components with pixel data"

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
+import Frame from './components/Frame'
+import { pixels } from './data/pixels'
 
 const App = () => {
   return (
-    <div className='bg-yellow-100 min-h-screen p-4 flex flex-col items-center justify-center'>
-      <div className='bg-yellow-600 inline-flex items-center justify-center p-11 border-b-[1rem] border-yellow-800'>
-        <div className='size-[30rem] grid grid-cols-12 grid-rows-12 border-t-[1rem] border-yellow-800 bg-white'>
-          {['bg-sky-950', 'bg-amber-950/50', 'bg-pink-700/40', 'bg-sky-950', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-sky-950', 'bg-pink-700/40', 'bg-amber-950/50', 'bg-sky-950', 'bg-sky-950', 'bg-sky-950', 'bg-amber-900/30', 'bg-amber-950/50', 'bg-pink-700/40', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-pink-700/40', 'bg-amber-950/50', 'bg-amber-900/30', 'bg-sky-950', 'bg-sky-950', 'bg-sky-950', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-sky-950', 'bg-sky-950', 'bg-sky-950', 'bg-amber-900/30', 'bg-blue-500/70', 'bg-blue-500/70', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-blue-500/70', 'bg-blue-500/70', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-sky-950', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-sky-300/40', 'bg-sky-300/40', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-sky-300/40', 'bg-sky-300/40', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-sky-950', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-pink-700/40', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-white', 'bg-pink-900/70', 'bg-pink-700/40', 'bg-pink-900/70', 'bg-white', 'bg-amber-900/30', 'bg-amber-900/30', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-white', 'bg-pink-700/40', 'bg-pink-700/40', 'bg-pink-700/40', 'bg-white', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-sky-950', 'bg-sky-950', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-sky-950', 'bg-sky-950', 'bg-sky-950', 'bg-sky-950', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-amber-950/50', 'bg-sky-950', 'bg-sky-950', 'bg-sky-950', 'bg-sky-950', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-amber-950/50', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-amber-950/50', 'bg-orange-300/70', 'bg-amber-950/50', 'bg-orange-300/70', 'bg-sky-950', 'bg-sky-950', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-orange-300/70', 'bg-sky-950'].map((pixel, index) => (
-            <span className={`${pixel} rounded-sm`} key={index}></span>
-          ))}
-        </div>
-      </div>
-      <div className='relative'>
-        <span className='absolute top-[1.7rem] right-[2.1rem] text-sm'>🤎</span>
-        <h5 className='text-center mt-8 text-xs text-amber-900'>Crafted with . by<br /> Frederick Moreno</h5>
-      </div>
+    <div className='p-12'>
+    <h5 className='text-3xl text-center font-medium text-amber-900'>Welcome to nowhere</h5>
+    <div className='flex justify-center flex-wrap gap-8 pt-12'>
+      <Frame variant={'sm'} pixel={pixels.RubberDucky} />
+      <Frame variant={'sm'} pixel={pixels.Zenitsu} />
+      <Frame variant={'sm'} pixel={pixels.Insouke} />
+    </div>
     </div>
   )
 }
