7d4dc60d2f32f80c39cf18d856386ce214fdbc58
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-12-13 12:26:02 +0800

    Enhance Modal and Collection components to display pixel details with name and description; refactor pixel data handling for improved structure

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Frame } from '../components'
 
-const Modal = ({ pixel, onClose }) => {
+const Modal = ({ pixel, name, desc, onClose }) => {
   return (
     <div className="bg-yellow-950/30 size-full fixed inset-0 -top-[5px] z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto">
-      <div className="sm:max-w-4xl sm:w-full m-3 sm:mx-auto">
+      <div className="sm:max-w-6xl sm:w-full m-4 sm:mx-auto">
         <div className="flex flex-col bg-white border shadow-sm rounded-xl">
           <div className="flex flex-col gap-4 items-center justify-center text-center p-7">
             <h4>Pixel Details</h4>
-            <p>Details about the selected pixel will be displayed here. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quam, sapiente in facere modi architecto ipsa a. Impedit nemo iste eius, ratione esse unde, repellat temporibus suscipit consequatur modi obcaecati? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor laudantium magnam necessitatibus quaerat laborum dolorem consequuntur officia natus repudiandae cupiditate quis, similique ex praesentium quae recusandae dolorum, sequi enim explicabo!</p>
+            <Frame variant={'lg'} pixel={pixel} />
+            <h5>{name}</h5>
+            <p>{desc}</p>
           </div>
           <div className="overflow-y-auto pb-7 flex justify-center gap-7">
             <div className="items-center gap-4 flex">
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,50 +1,66 @@
-import React, { useState } from 'react'
-import { Frame, Modal } from '../components'
-import { pixels } from '../data/pixels'
-
-const pixelData = [
-  pixels.RubberDucky, pixels.Zenitsu, pixels.Insouke, pixels.NezukoXTanjiro, pixels.DeadpoolXWolverine, pixels.Rengoku, pixels.Zenitsu, pixels.Insouke, pixels.NezukoXTanjiro, pixels.DeadpoolXWolverine, pixels.Rengoku,
-]
+import React, { useState } from 'react';
+import { Frame, Modal } from '../components';
+import { Data } from '../data/pixels';
 
 const Collection = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [selectedPixel, setSelectedPixel] = useState(null)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPixel, setSelectedPixel] = useState(null);
 
   const handleFrameClick = (pixel) => {
-    setSelectedPixel(pixel)
-    setIsModalOpen(true)
-  }
+    setSelectedPixel(pixel);
+    setIsModalOpen(true);
+  };
 
   const closeModal = () => {
-    setIsModalOpen(false)
-    setSelectedPixel(null)
-  }
+    setIsModalOpen(false);
+    setSelectedPixel(null);
+  };
 
   return (
-    <section className='p-1 space-y-[4.5px]'>
-      <div className='flex gap-2'>
-        <div className='flex justify-center items-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-yellow-800 mx-1">
+    <section className="p-1 space-y-[4.5px]">
+      <div className="flex gap-2">
+        <div className="flex justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-yellow-800 mx-1"
+          >
             <path d="M5.566 4.657A4.505 4.505 0 0 1 6.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0 0 15.75 3h-7.5a3 3 0 0 0-2.684 1.657ZM2.25 12a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3v-6ZM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 0 1 6.75 6h10.5a3 3 0 0 1 2.683 1.657A4.505 4.505 0 0 0 18.75 7.5H5.25Z" />
           </svg>
         </div>
-        <div className='w-full bg-yellow-200 border-4 border-yellow-300/50 ring-1 ring-amber-500 pixel-font text-yellow-800'>Collection</div>
+        <div className="w-full bg-yellow-200 border-4 border-yellow-300/50 ring-1 ring-amber-500 pixel-font text-yellow-800">
+          Collection
+        </div>
       </div>
-      <div className='p-4 bg-[#fef6b2] border-4 border-yellow-200/80 ring-1 ring-amber-500'>
-        <div className='flex flex-col items-center justify-center mb-4'>
+      <div className="p-4 bg-[#fef6b2] border-4 border-yellow-200/80 ring-1 ring-amber-500">
+        <div className="flex flex-col items-center justify-center mb-4">
           <h5>Collections</h5>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati fugi</p>
         </div>
-        <div className='flex justify-center flex-wrap gap-4'>
-          {pixelData.map((item, index) => (
-            <Frame key={index} variant={'sm'} pixel={item} onClick={() => handleFrameClick(item)} />
+        <div className="flex justify-center flex-wrap gap-4">
+          {Object.keys(Data).map((category) => (
+            Data[category].map((item, index) => (
+              <Frame
+                key={`${category}-${index}`}
+                variant={'sm'}
+                pixel={item.pixels}
+                onClick={() => handleFrameClick(item)}
+              />
+            ))
           ))}
         </div>
       </div>
-      {isModalOpen && <Modal pixel={selectedPixel} onClose={closeModal} />}
+      {isModalOpen && (
+        <Modal
+          pixel={selectedPixel.pixels}
+          name={selectedPixel.name}
+          desc={selectedPixel.description}
+          onClose={closeModal}
+        />
+      )}
     </section>
-  )
-}
+  );
+};
 
-
-export default Collection
+export default Collection;
